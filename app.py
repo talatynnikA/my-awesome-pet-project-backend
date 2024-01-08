3c9fd5060627f618e8fd8361f908294973b9b0ef
Artyom Talatynnik <a.talatynnik@onesoil.ai>
2024-01-08 13:31:11 +0100

    add add flask migrate

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ from flask import Flask, render_template, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_restful import Api, Resource
 from collections import OrderedDict
+from flask_migrate import Migrate
+
+
+
 import os
 
 db_username = os.environ.get('DB_USERNAME')
@@ -18,6 +22,7 @@ app = Flask(__name__, template_folder='templates')
 app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{db_username}:{db_password}@localhost:5432/{db_uri}'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 api = Api(app)
 
 class Contact(db.Model):
@@ -84,44 +89,26 @@ def init_data():
 
 @app.route('/')
 def index():
-    init_data()  # Инициализация данных
+    init_data()  # data init
     contacts = Contact.query.all()
     contact_list = [{"id": contact.id, "name": contact.name, "email": contact.email} for contact in contacts]
 
+    # request data from table AboutMe
+    about_me_data = AboutMe.query.all()
+    about_me_dict = {entry.section: entry.content for entry in about_me_data}
+
+    # insert data from table AboutMe to response_data
     response_data = OrderedDict([
-        ('Hello', [
-            'My name is',
-            'Artyom Talatynnik!',
-            'This is my demo project',
-            'Welcome!',
-        ]),
-        ('ENV', 'test'),
-        ('Skills', [
-            'DevOps Technologies: Kubernetes, GitLab, Terraform, AWS',
-            'Operating Systems: Linux (Ubuntu, CentOS)',
-            'Programming Languages: C, C++, C#, Python',
-            'Web Development: HTML, HTML5, JavaScript (JS), jQuery',
-            'Databases: SQL (MySQL, PostgreSQL, MSSQL, Oracle)',
-            'Version Control: Git, GitLab',
-            'CI/CD: Gitlab CI, Jenkins',
-            '3D Modeling and Design: Unity, 3ds Max',
-        ]),
-        ('Education', "BELARUSIAN STATE TECHNOLOGICAL UNIVERSITY\nFaculty of Information Technology - 2019-2023\nBachelor's Degree in Information Technology"),
-        ('Experience', [
-            "2021 - 2022 | System Administrator | Belarussian State Technological University\nSet up a network for windows computers to transfer files over FTP protocol and to use RDP connection. Ensuring data security and backup. Manage and record lessons for a variety of IT topics: Cyber Security, Cryptography, Unity Engine, CSS, HTML, JavaScript. Use git for VCS in Adobe Premiere projects, Install and manage OS Windows for video production.",
-            "2023 Jul – Present Time | Junior DevOps | Onesoil\nManaging Gitlab CI Pipelines, Setting up Cloudfront Distributions with IaC (Terraform, Terragrunt) and other infrastructure for them, managing Kubernetes cluster, Working with docker images in AWS ECR, creating AWS CloudWatch alarms and other AWS services.",
-        ]),
-        ('About me', [
-            '1.5+ years of experience in IT. Self-driven and results-oriented.',
-            'Recently graduated from Belarusian State Technological University with a degree in Information Technology where started my career as a System Administrator.',
-            'Currently serving as a DevOps Intern at Onesoil, where I apply my knowledge and skills in optimizing Software Development processes and Cloud Automation.',
-            'Eager to contribute my expertise and continue learning in a dynamic DevOps environment.',
-            'Well-versed in technologies such as Kubernetes, GitLab, Terraform, AWS, and Linux.',
-        ]),
+        ('Hello', about_me_dict.get('Hello', [])),
+        ('Skills', about_me_dict.get('Skills', [])),
+        ('Education', about_me_dict.get('Education', '')),
+        ('Experience', about_me_dict.get('Experience', [])),
+        ('About me', about_me_dict.get('About me', [])),
         ('Contacts', contact_list),
     ])
 
     return jsonify(response_data)
+
 
 @app.route('/contacts')
 def contacts():
